2a36cfaaaf8cd306f5cfc4e6285c7f7ad02b5935
Ryan Rushton <ryan.rushton79@gmail.com>
2018-06-07 23:26:05 +1000

    Added some debug config for vs code. Changed the container test to use a smaller dummy data for testing. Added snapshots.

--- xbdd-web/src/__tests__/tag-list/containers/TagListContainer-test.js
+++ xbdd-web/src/__tests__/tag-list/containers/TagListContainer-test.js
@@ -3,16 +3,24 @@ import { shallow } from 'enzyme';
 import Report from '../../../models/Report';
 import TagListContainer from '../../../modules/tag-list/containers/TagListContainer';
 import TagListView from '../../../modules/tag-list/components/TagListView';
-import dummyData from '../../../resources/tag-response.json';
+
+const dummyReport = new Report([{
+    tag: 'testTagOne',
+    features: [{
+        elements: [{
+            steps: [{
+                result: { status: 'passed' },
+            }],
+        }],
+    }],
+}]);
 
 test('TagListContainer renders', () => {
-    const dummyReport = new Report(dummyData);
     const tagList = shallow(<TagListContainer report={dummyReport} />);
     expect(tagList).toMatchSnapshot();
 });
 
 test('TagListContainer renders the TagListView', () => {
-    const dummyReport = new Report(dummyData);
     const tagList = shallow(<TagListContainer report={dummyReport} />);
     expect(tagList.find(TagListView)).toHaveLength(1);
 });
