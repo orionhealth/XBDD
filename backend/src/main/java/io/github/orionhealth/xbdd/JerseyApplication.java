a41e23acec0ae82d739eb60d5db88c5fa186a7ae
Ryan Rushton <ryan.rushton79@gmail.com>
2020-04-17 10:10:25 +1000

    Added response compression to the jersey resources. There is a factor of about 10 for size reduction of the large json files.

--- backend/src/main/java/io/github/orionhealth/xbdd/JerseyApplication.java
+++ backend/src/main/java/io/github/orionhealth/xbdd/JerseyApplication.java
@@ -18,9 +18,11 @@ package io.github.orionhealth.xbdd;
 import javax.ws.rs.ApplicationPath;
 
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.message.GZipEncoder;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
 import org.glassfish.jersey.server.TracingConfig;
+import org.glassfish.jersey.server.filter.EncodingFilter;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -29,6 +31,8 @@ public class JerseyApplication extends ResourceConfig {
 
 	public JerseyApplication() {
 		packages(getClass().getPackage().getName());
+
+		EncodingFilter.enableFor(this, GZipEncoder.class);
 
 		register(MultiPartFeature.class);
 
