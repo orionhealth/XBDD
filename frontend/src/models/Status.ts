eb054412446d6abdb5f34e867728a4f96e4effdf
Ryan Rushton <ryan.rushton79@gmail.com>
2020-04-30 11:19:51 +1000

    Fixed issue call was failing due to a missing error type. We have no need for that error type so I am mapping everything to undefined that isn't found.

--- frontend/src/models/Status.ts
+++ frontend/src/models/Status.ts
@@ -12,7 +12,7 @@ export const { Passed, Failed, Skipped, Undefined } = Status;
 export const getStatusFromString = (statusString: string): Status => {
   const res = Object.values(Status).find(status => status === statusString);
   if (!res) {
-    throw new Error(`No Status exists for string ${statusString}`);
+    return Status.Undefined;
   }
   return res;
 };
